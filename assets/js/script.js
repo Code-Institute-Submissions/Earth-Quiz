2c85ca216dc82ee6cf5050203f634d1f8ef4b2ac
todorr92 <todorovicslobodan10@gmail.com>
2020-06-13 09:56:00 +0000

    added cursor pointer to answer btns and removed focus once clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
-$(document).ready(function{
-    $(".quiz-questions").hide();
+document.getElementsByClassName("answer-btn");
+
+let answersClicked = [
+    {answer: "1. The Pacific Ocean", correct: true},
+    {answer: "2. The Indian Ocean", correct: false},
+    {answer: "3. The Black Sea", correct: false},
+    {answer: "4. The Arctic Ocean", correct: false}
+]
+
+//hide question section when the page is loaded
+$(document).ready(function(){
+    $(".question").hide();
 })
+
+//on click Start Quiz button hide landing page and show question 1 in quiz
+$("#start-button").click(function(){
+    $("#landing-page").hide("slow");
+    $("#question-1").show("slow");
+})
+
+
+function clickedAnswer(){
+    $(".answer-btn").click(function(){
+        if ($(this) === answersClicked.correct(true)) {
+            $(this).css("background-color", "green");
+        }
+        else {
+            $(this).css("background-color", "red");
+        }
+    })
+}
+
